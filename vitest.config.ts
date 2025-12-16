e44f05a0952634e18714285e5e704562edfe6594
jey3dayo <nakazato_junya@ca-adv.co.jp>
2025-12-16 11:44:21 +0900

    chore: add prettier config and format scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: "jsdom",
-    include: ["tests/**/*.test.ts"],
+    environment: 'jsdom',
+    include: ['tests/**/*.test.ts'],
     clearMocks: true,
   },
 });
-
